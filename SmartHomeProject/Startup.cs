ae45ac747c319bb7aba230e438905bcc8326d8f0
fırat özcevahir <firat.ozcevahir@gmail.com>
2019-10-12 09:55:27 +0300

    UI Updates
    -Some UI Updates

--- SmartHomeProject/Startup.cs
+++ SmartHomeProject/Startup.cs
@@ -37,6 +37,16 @@ namespace SmartHomeProject
             services.AddRazorPages();
             services.AddSignalR();
             services.AddAntiforgery(o => o.HeaderName = "XSRF-TOKEN");
+            
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AutomaticAuthentication = false;
+            });
+
+            services.Configure<IISOptions>(options =>
+            {
+                options.ForwardClientCertificate = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
